f52f7aed4a566a91f8b4fb94645d680406c896d5
zhiyangdai <zhiyang.dai@transwarp.io>
2020-07-21 15:38:28 +0800

    fix(pkg/release/usecase/helm/template.go): fix for buildReleaseInfo with dryrun releaseCache

--- pkg/release/usecase/helm/template.go
+++ pkg/release/usecase/helm/template.go
@@ -5,6 +5,8 @@ import (
 	k8sModel "WarpCloud/walm/pkg/models/k8s"
 	"WarpCloud/walm/pkg/models/release"
 	"WarpCloud/walm/pkg/release/utils"
+	"encoding/json"
+	"github.com/tidwall/gjson"
 	"k8s.io/klog"
 )
 
@@ -25,15 +27,31 @@ func (helm *Helm) DryRunRelease(namespace string, releaseRequest *release.Releas
 }
 
 func (helm *Helm) DryRunUpdateRelease(namespace string, releaseRequest *release.ReleaseRequestV2, chartFiles []*common.BufferedFile) ([]*k8sModel.ReleaseConfig, error) {
-	releaseCache, err := helm.doInstallUpgradeRelease(namespace, releaseRequest, chartFiles, true)
+	releaseCache, err := helm.doInstallUpgradeRelease(namespace, releaseRequest, nil, true)
 	if err != nil {
 		klog.Errorf("failed to dry run install release : %s", err.Error())
 		return nil, err
 	}
-	releaseInfo, err := helm.buildReleaseInfoV2(releaseCache)
+	resources, err := helm.k8sOperator.BuildManifestObjects(namespace, releaseCache.Manifest)
 	if err != nil {
-		klog.Errorf("failed to build releaseInfo: %s", err.Error())
+		klog.Errorf("failed to build unstructured : %s", err.Error())
 		return nil, err
+	}
+	outputConfigValues := map[string]interface{}{}
+	for _, resource := range resources {
+		if resource["kind"] == "ReleaseConfig" {
+			resourceData, err := json.Marshal(resource)
+			if err != nil {
+				klog.Errorf("failed to marshal k8s resource: %s", err.Error())
+				return nil, err
+			}
+			outputConfigStr := gjson.Get(string(resourceData), "spec.outputConfig").String()
+			err = json.Unmarshal([]byte(outputConfigStr), &outputConfigValues)
+			if err != nil {
+				klog.Errorf("failed to unmarshal outputConfig: %s", err.Error())
+			}
+			break
+		}
 	}
 
 	oldReleaseInfo, err := helm.GetRelease(releaseCache.Namespace, releaseCache.Name)
@@ -41,7 +59,7 @@ func (helm *Helm) DryRunUpdateRelease(namespace string, releaseRequest *release.
 		return nil, err
 	}
 	var dependedReleases []*k8sModel.ReleaseConfig
-	if utils.ConfigValuesDiff(oldReleaseInfo.OutputConfigValues, releaseInfo.OutputConfigValues) {
+	if utils.ConfigValuesDiff(oldReleaseInfo.OutputConfigValues, outputConfigValues) {
 		releaseConfigs, err := helm.k8sCache.ListReleaseConfigs("", "")
 		if err != nil {
 			klog.Errorf("failed to list releaseconfigs: %s", err.Error())
@@ -53,13 +71,12 @@ func (helm *Helm) DryRunUpdateRelease(namespace string, releaseRequest *release.
 				if err != nil {
 					continue
 				}
-				if dependedReleaseNamespace == releaseInfo.Namespace && dependedReleaseName == releaseInfo.Name {
+				if dependedReleaseNamespace == releaseCache.Namespace && dependedReleaseName == releaseCache.Name {
 					dependedReleases = append(dependedReleases, releaseConfig)
 				}
 			}
 		}
 	}
-
 	return dependedReleases, nil
 }
 
